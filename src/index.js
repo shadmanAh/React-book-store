d35fa9533ba91dc48d66c6322de39dfef3c012d4
shadmanAh <shadman061@gmail.com>
2023-05-31 18:01:12 +0330

    remove fils

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  const image =
+    'https://dkstatics-public.digikala.com/digikala-products/eb896e9c50a4eae5d975f36ee1e275c2a2216a71_1674388725.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/format,webp/quality,q_90'
+  const title = 'دختری که به اعماق دریا افتاد'
+  const author = 'اکسی اوه'
+  return (
+    <article className="book">
+      <img src={image} />
+      <h2>{title}</h2>
+      <h4> {author}</h4>
+    </article>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<BookList />)
